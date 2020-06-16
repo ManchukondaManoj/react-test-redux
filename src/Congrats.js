263d4702c466426eb63a88e6473a837966798bea
ManchukondaManoj <manchukondamanoj@gmail.com>
2020-06-16 17:13:21 +0530

    Added bootstrap

--- src/Congrats.js
+++ src/Congrats.js
@@ -4,9 +4,11 @@ const Congrats = (props) => {
     if (props.success) {
         return (
             <div data-test="component-congrats">
-                <span data-test="congrats-message">
+                <span data-test="congrats-message" className="alert alert-success">
                     Congratulations! You guessed the word!
                 </span>
+                <br />
+                <br />
             </div>
         )
     } else {
