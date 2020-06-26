bf264a1ddc879993c7c6a0d2e554267f686b5790
ManchukondaManoj <manchukondamanoj@gmail.com>
2020-06-26 10:41:02 +0530

    Set up Input component, storeFactory test util and fix reducer bug

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,6 +1,20 @@
-import { ShallowWrapper } from "enzyme"
-
+import { ShallowWrapper } from "enzyme";
+import { createStore } from "redux";
 import checkPropTypes from "check-prop-types";
+
+import rootReducer from "../src/reducers";
+
+
+/**
+ * Create a testing store with imported reducers, middlewares, and initial state.
+ * globals:rootReducer
+ * @param {object} initalState - initial state for store
+ * @function storeFactory
+ * @returns {Store} - Redux store
+ */
+export const storeFactory = (initalState = {}) => {
+    return createStore(rootReducer, initalState);
+}
 
 /**
  * Return node(s) with the given data-test attribute.
